5d03036a50cd02af8e9fd19d9bf53ab33ca32f40
Tausif Solaiman <tausifsolaiman@gmail.com>
2019-10-19 15:48:33 -0400

    solrad component

---
+++ src/Views/SolarRad.js
@@ -0,0 +1,35 @@
+import React from 'react'
+import axios from 'axios'
+
+class SolarRad extends React.Component {
+
+  constructor () {
+    super()
+    this.state = {
+      solrad: {outputs: {solrad_monthly: []}}
+    }
+  }
+
+ async componentDidMount() {
+    const solarRad = await axios.get('https://developer.nrel.gov/api/pvwatts/v6.json?api_key=GDegXZpZdwcvtgRxy4bovbrVtN6NbLTV9UDBpRyo&lat=40&lon=-105&system_capacity=4&azimuth=180&tilt=40&array_type=1&module_type=1&losses=10')
+    console.log(solarRad.data)
+
+    this.setState({solrad: solarRad.data})
+  }
+
+  render() {
+    return (
+    <div>
+      <h1>Solar Radiation</h1>
+      <ul>
+        {this.state.solrad.outputs.solrad_monthly.map(rad => {
+        return <li>{rad}</li>
+        })}
+      </ul>
+    </div>
+    )
+  }
+  
+}
+
+export default SolarRad
