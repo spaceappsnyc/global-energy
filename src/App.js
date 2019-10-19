0ac952c8feee48d09d59149ea03c79408eaaddee
Nayyif Oussamatou <aremoustar@gmail.com>
2019-10-19 15:05:10 -0400

    About route disabled

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import {Route} from 'react-router-dom'
+import Map from './Map'
 
 function App() {
   return (
     <div className="App">
-      <Route path = '/about' component={About} />
+      <Route exact path = '/' component = {Map} />
+      {/* <Route path = '/about' component={About} /> */}
     </div>
   );
 }
