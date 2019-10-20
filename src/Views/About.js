717fc0239938d078089c61fd24dd3228146bfcb3
droza0 <droza0@users.noreply.github.com>
2019-10-20 11:26:21 -0400

    things

--- src/Views/About.js
+++ src/Views/About.js
@@ -4,7 +4,7 @@ const About = () => {
     return (
         <div className='about-page'>
             <h1>About</h1>
-            <p>Global Energy is on a mission to drastically reduce CO2 emissions by giving people a nudge towards using solar energy by seeing how much money they would save. We also help industry leaders find spots with great solar coverage to build cutting-edge solar farms.</p>
+            <p>Global Energy is on a mission to drastically reduce CO2 emissions by giving people a nudge towards using solar energy by seeing how much money they would save. Global citizens can trust us because we have no profit motive and are 100% transparent and open source.  We also help industry leaders find spots with great solar coverage to build cutting-edge solar farms.</p>
         </div>
     )
 }
