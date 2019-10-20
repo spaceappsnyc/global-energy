2ffaf770d125a4c6e132b961bba0bcf947254da9
Nendae <ttp_laptop@TTP-Laptops-Air.fios-router.home>
2019-10-19 20:46:10 -0400

    added SolarCalc component to the SolarRad view in order to pass the props

--- src/Views/SolarCalc.js
+++ src/Views/SolarCalc.js
@@ -5,28 +5,68 @@ class SolarCalc extends React.Component {
     constructor() {
         super();
         this.state = {
-            solarData: {},
+            energy: 0,
+            solarRadiation: 0,
+            panelArea: 0,
+            performanceRatio: .15
         }
     }
 
-    async componentDidMount () {
-        console.log("made the call")
-        const solarResponse = await axios.get('https://developer.nrel.gov/api/pvwatts/v6.json?api_key=GDegXZpZdwcvtgRxy4bovbrVtN6NbLTV9UDBpRyo&lat=40.7698823&lon=-73.9656831&system_capacity=4&azimuth=180&tilt=40&array_type=1&module_type=1&losses=10')
-        this.setState({solarData: solarResponse.data})
-        console.log("The state is", this.state)
-    }
+calcSolarProduction (){
 
-    render() {
-        const {ac_monthly, dc_monthly} = this.state.solarData.outputs
-        return (
-            <div className='electricity'>
-                <h1>Electrical Data</h1>
-                <ul>
-                    {ac_monthly.map((output, index) => <li key = {index}>{output}</li>)}
-                </ul>
-            </div>
-        )
-    }
+
+
+}
+
+calcSolarSavings (){
+
+
+
+}
+
+calcSolarCosts (){
+
+
+
+}
+
+calcSolarPayoff(){
+
+
+
+}
+
+
+
+/*
+E = A * r * H * PR
+
+E = Energy (kWh)
+A = Total solar panel Area (m2)
+r = solar panel yield or efficiency(%)
+H = Annual average solar radiation on tilted panels (shadings not included)
+PR = Performance ratio, coefficient for losses (range between 0.5 and 0.9, default value = 0.75)
+
+*/
+
+//     async componentDidMount () {
+//         console.log("made the call")
+//         const solarResponse = await axios.get('https://developer.nrel.gov/api/pvwatts/v6.json?api_key=GDegXZpZdwcvtgRxy4bovbrVtN6NbLTV9UDBpRyo&lat=40.7698823&lon=-73.9656831&system_capacity=4&azimuth=180&tilt=40&array_type=1&module_type=1&losses=10')
+//         this.setState({solarData: solarResponse.data})
+//         console.log("The state is", this.state)
+//     }
+
+//     render() {
+//         const {ac_monthly, dc_monthly} = this.state.solarData.outputs
+//         return (
+//             <div className='electricity'>
+//                 <h1>Electrical Data</h1>
+//                 <ul>
+//                     {ac_monthly.map((output, index) => <li key = {index}>{output}</li>)}
+//                 </ul>
+//             </div>
+//         )
+//     }
 
 }
 
--- src/Views/SolarRad.js
+++ src/Views/SolarRad.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import axios from 'axios'
 import {convertSolarRads} from '../Services/SolarRad/Utility'
+import SolarCalc from './SolarCalc.js'
 
 class SolarRad extends React.Component {
 
@@ -12,7 +13,7 @@ class SolarRad extends React.Component {
   }
 
  async componentDidMount() {
-   
+
     const solarRad = await axios.get('https://developer.nrel.gov/api/pvwatts/v6.json?api_key=GDegXZpZdwcvtgRxy4bovbrVtN6NbLTV9UDBpRyo&lat=40&lon=-105&system_capacity=4&azimuth=180&tilt=40&array_type=1&module_type=1&losses=10')
 
     const coverageFeedback = convertSolarRads(solarRad.data)
@@ -26,6 +27,7 @@ class SolarRad extends React.Component {
     return (
     <div>
       <h1>Solar Radiation</h1>
+      <SolarCalc solarData = {this.state}></SolarCalc>
       <ul>
         {this.state.solrad.outputs.solrad_monthly.map(rad => {
         return (
@@ -36,7 +38,7 @@ class SolarRad extends React.Component {
     </div>
     )
   }
-  
+
 }
 
 export default SolarRad
